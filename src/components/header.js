0a4a67f238850af2b77521d8f6a7ca58ab788a31
Rajat Sharma <lunasunkaiser@gmail.com>
2017-07-03 17:55:07 +0530

    Added Github Icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import Github from '../../public/github.png'
 
 const StyledHeader = styled.div `
   background-color:${props => props.color};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   font-size: 30px;
   padding: 10px;
   padding-left: 30px;
+  padding-right: 30px;
   color:#fff;
 `
 
-const AppHeader = props => <StyledHeader color={props.colors.Primary}>Faker</StyledHeader>
+const GithubImage = styled.img`
+  height: 30px;
+`
+
+const AppHeader = props => <StyledHeader color={props.colors.Primary}>
+  <div>Faker</div>
+  <a href='https://github.com/rajatsharma305/faker' target='_blank'><GithubImage src={Github} /></a>
+  </StyledHeader>
 
 export default AppHeader
